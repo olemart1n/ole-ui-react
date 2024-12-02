47f61f3e882b19bc7a4d43392d90447eba1b7e47
ole martin <olesnoen@gmail.com>
2024-12-03 00:21:18 +0100

    typing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: "lib/index.ts",
